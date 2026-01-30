96b3bd445f4e3d48659e4769ffa587e76c650005
Beatriz Moreira <bbmmegg@gmail.com>
2026-01-30 15:53:02 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,10 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : "+ chessMatch.getTurn());
 		System.out.println("Waiting player : "+ chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
+		
 	}
 	
 
@@ -112,6 +116,7 @@ public class UI {
 		System.out.println(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.println(ANSI_RESET);
+		System.out.println("Black : ");
 		System.out.println(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
 		
